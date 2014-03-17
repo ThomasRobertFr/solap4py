4fc0912ee991f21d6400b6516c3a36d6a3044402
idaoudi <daoudi.ibrahim@gmx.fr>
2014-03-17 15:13:21 +0100

    Java main class constructor initialization

--- src/core/Solap4py.java
+++ src/core/Solap4py.java
@@ -2,14 +2,32 @@ package src.core;
 
 
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 import javax.json.*;
-import org.olap4j.*;
+
+import org.olap4j.OlapConnection;
 
 
 public class Solap4py {
 
+	private OlapConnection olapConnection;
 	
 	public Solap4py() {
+		try {
+			Class.forName("org.olap4j.driver.xmla.XmlaOlap4jDriver");
+			
+			Connection connection = DriverManager.getConnection("jdbc:xmla:Server=http://postgres:westcoast@192.168.1.1:8080/geomondrian/xmla");
+			
+			this.olapConnection = connection.unwrap(OlapConnection.class);
+			
+		} catch (ClassNotFoundException e) {
+			System.err.println(e);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 		
 	}
 	
