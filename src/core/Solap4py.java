950e4445bbb6505f998ab4e50591ee6f39d78ecc
rchevalier <rchevalier@westpic-rchevalier>
2014-03-17 13:32:54 +0100

    test

--- src/core/Solap4py.java
+++ src/core/Solap4py.java
@@ -1,8 +1,23 @@
 package core;
 
+
+import org.olap4j.*;
+
+
+
 public class Solap4py {
 
 	
+	public Solap4py() {
+		
+	}
 	
+	public void select() {
+		
+	}
+	
+	public void getMetadata() {
+		
+	}
 	
 }
