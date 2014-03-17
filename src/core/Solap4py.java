da0e7b2810fe54bc6c58c9f4b67b87c75be2ba88
Rémy Chevalier <remychevalier@laposte.net>
2014-03-17 15:30:04 +0100

    getMetadata

--- src/core/Solap4py.java
+++ src/core/Solap4py.java
@@ -1,13 +1,20 @@
 package src.core;
 
+import java.io.StringReader;
 
-
-import javax.json.*;
-import org.olap4j.*;
+import javax.json.Json;
+import javax.json.JsonArray;
+import javax.json.JsonObject;
+import javax.json.JsonObjectBuilder;
+import javax.json.JsonString;
 
 
 public class Solap4py {
 
+	
+	
+	static final String[] levels = { "schema", "cube", "dimension"  };
+	
 	
 	public Solap4py() {
 		
@@ -17,8 +24,31 @@ public class Solap4py {
 		
 	}
 	
-	public void getMetadata() {
+	public String getMetadata(String param) {
 		
+		JsonObject query = Json.createReader(new StringReader(param)).readObject();
+		JsonObjectBuilder result = Json.createObjectBuilder();
+
+		for(String level : Solap4py.levels) {
+			JsonArray array = query.getJsonArray(level);
+			for(JsonString element : array.getValuesAs(JsonString.class)) {
+				
+			}
+		}
+		
+		
+		return result.build().toString();
+	}
+	
+	
+	public static void main(String[] args) {
+		
+		String query = "{\"schema\" : [ \"Traffic\"],\"cube\" : [],\"dimension\" : [],\"measure\" : [],\"hierarchy\" : [],\"level\" : [],\"member\" : [],\"property\" : []}";
+		
+		Solap4py p = new Solap4py();
+		String metadata = p.getMetadata(query);
+	
+		System.out.println(metadata);
 	}
 	
 }
