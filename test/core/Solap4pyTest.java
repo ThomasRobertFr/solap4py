5fcd1d7e473ba7317d9cae07a321785bd159b8ae
yportailler <youri.portailler@insa-rouen.fr>
2014-03-20 15:33:23 +0100

    révision architecture dossier tests

---
+++ test/core/Solap4pyTest.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package core;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * @author zangetsu
+ *
+ */
+public class Solap4pyTest {
+
+	@Test
+	public void test() {
+		assertEquals(true,true);
+	}
+
+}
