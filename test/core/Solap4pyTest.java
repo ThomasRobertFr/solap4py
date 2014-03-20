bf80e78c72b500513aec70ab788cb77e1b7993cc
yportailler <youri.portailler@insa-rouen.fr>
2014-03-20 16:08:04 +0100

    Solap4pyTest ajouts de fonctions test

--- test/core/Solap4pyTest.java
+++ test/core/Solap4pyTest.java
@@ -5,6 +5,7 @@ package core;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
 /**
@@ -13,8 +14,20 @@ import org.junit.Test;
  */
 public class Solap4pyTest {
 
+	Solap4py s;
+	
+	@Before
+	public void avantTest(){
+		s = new Solap4py();
+	}
+	
+	
 	@Test
-	public void test() {
+	public void testSelect(){
+		assertEquals(true,true);
+	}
+	@Test
+	public void testGetMetadata(){
 		assertEquals(true,true);
 	}
 
