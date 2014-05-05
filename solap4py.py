0cad1aabb2180ce1536dbdda175d5475cfaec62f
Rémy Chevalier <remychevalier@laposte.net>
2014-05-05 09:23:53 +0200

    replaced py4j with sockets

--- solap4py.py
+++ solap4py.py
@@ -1,18 +1,33 @@
-from py4j.java_gateway import JavaGateway, GatewayClient
-gateway = JavaGateway(GatewayClient(port=25335))
+import socket
 
-solap4py = gateway.entry_point.getSolap4py();
+#from py4j.java_gateway import JavaGateway, GatewayClient
+#gateway = JavaGateway(GatewayClient(port=25335))
+
+#solap4py = gateway.entry_point.getSolap4py();
 
 
 def process(strQuery):
     # Converts the input string (formatted as a json text) to a byte array
-    bytes = bytearray(strQuery)
+    #bytes = bytearray(strQuery)
 
     # Process the query and get the resulting byte array
-    result = solap4py.process(bytes)
+    #result = solap4py.process(bytes)
 
     # Decode the byte array with utf-8
-    utf8Result = result.decode('utf-8')
+    #utf8Result = result.decode('utf-8')
     
     # Return the resulted string
-    return utf8Result;
+    #return utf8Result;
+
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect(("localhost", 25335)) # TODO which port should we use ?
+
+    s.send(strQuery + '\r\n') 
+    data = bytearray()
+    while 1:
+        chunk = s.recv(65536)
+        if not chunk:
+            break
+        data.extend(chunk)
+    
+    return data.decode(encoding='utf-8')
