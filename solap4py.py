a2930951db592230f1c855af2475e722916eec28
Ibrahim Daoudi <daoudi.ibrahim@gmx.fr>
2014-05-07 09:08:59 +0200

    Set encoding to utf8

--- solap4py.py
+++ solap4py.py
@@ -1,24 +1,7 @@
+# -*- coding: utf8 -*-
 import socket
 
-#from py4j.java_gateway import JavaGateway, GatewayClient
-#gateway = JavaGateway(GatewayClient(port=25335))
-
-#solap4py = gateway.entry_point.getSolap4py();
-
-
 def process(strQuery):
-    # Converts the input string (formatted as a json text) to a byte array
-    #bytes = bytearray(strQuery)
-
-    # Process the query and get the resulting byte array
-    #result = solap4py.process(bytes)
-
-    # Decode the byte array with utf-8
-    #utf8Result = result.decode('utf-8')
-    
-    # Return the resulted string
-    #return utf8Result;
-
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.connect(("localhost", 25335)) # TODO which port should we use ?
 
